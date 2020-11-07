a881a72667f155c704620665077d0dab3aad019f
kylevh <kylehmizu@gmail.com>
2020-11-06 21:12:24 -0800

    friday night's upload v1

---
+++ OZP_UWProject/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+
+    public float movementSpeed = 5f;
+    Vector2 movement;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        DoTheMovementThing();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void DoTheMovementThing() //does the movement thing
+    {
+        //Input
+        if (Mathf.Abs(movement.y) >= .7f && Mathf.Abs(movement.x) >= .7f)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal") * .7f;
+            movement.y = Input.GetAxisRaw("Vertical") * .7f;
+        }
+        else
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Magnitude", movement.magnitude);
+    }
+}
