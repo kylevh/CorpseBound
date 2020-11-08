6714f1d766167064920286738ca6830118649594
ajayk111 <ajayk111@users.noreply.github.com>
2020-11-07 19:25:34 -0500

    Frat boy changes

---
+++ OZP_UWProject/Assets/Scripts/FratBoy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FratBoy : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    
+    private static Tilemap tilemap;
+    private AIPath aiPath;
+    private Rigidbody2D rb2d;
+
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int X = Animator.StringToHash("X");
+    private static readonly int Y = Animator.StringToHash("Y");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        aiPath = GetComponent<AIPath>();
+        if (!tilemap)
+        {
+            tilemap = GameObject.FindObjectOfType<Tilemap>();
+            
+        }
+
+        transform.position = tilemap.AlignToGrid(transform.position) + new Vector3(0,0,-0.01f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat(Speed,aiPath.desiredVelocity.sqrMagnitude);
+        anim.SetFloat(X,aiPath.desiredVelocity.x);
+        anim.SetFloat(Y,aiPath.desiredVelocity.y);
+    }
+}
---
+++ OZP_UWProject/Assets/Scripts/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class Utilities
+{
+    public static Vector3 AlignToGrid(this Tilemap tilemap, Vector3 vec)
+    {
+        return tilemap.CellToWorld(tilemap.WorldToCell(vec));
+    }
+}
