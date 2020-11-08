dc36368617e8fa8a84d8d7bde8b87b1d96820969
kylevh <kylehmizu@gmail.com>
2020-11-07 19:40:48 -0800

    start of dialogue and sound

---
+++ OZP_UWProject/Assets/Scripts/DialogueLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header("Text Options")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Timer parameters")]
+        [SerializeField] private float delay;
+
+        [Header("Sound")]
+        [SerializeField] private AudioClip sound;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, sound));
+        }
+
+    }
+}
